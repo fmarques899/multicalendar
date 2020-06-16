c1bd1362d815e3b85a64766051ae9c9e2e4108fb
Felipe Sampaio Marques <fmarques899@gmail.com>
2020-06-16 00:30:15 -0300

    Updating angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,58 +11,58 @@ export class AppComponent {
 
   public apartments: Apartment[] = [
     {
-      "id": 1,
-      "title": "Casa SHIN Qi 12 Lote 7, 11",
-      "reservations": [{
-        checkin: new Date("2020-06-01"),
-        checkout: new Date("2020-06-08"),
+      id: 1,
+      title: 'Casa SHIN Qi 12 Lote 7, 11',
+      reservations: [{
+        checkin: new Date('2020-06-16'),
+        checkout: new Date('2020-06-18'),
         guest: {
-          name: "Sam Rothstein",
-          phone: "5561981494249"
+          name: 'Sam Rothstein',
+          phone: '5561981494249'
         },
-        color: "#f54242"
+        color: '#f54242'
       },
       {
-        checkin: new Date("2020-06-09"),
-        checkout: new Date("2020-06-10"),
+        checkin: new Date('2020-06-19'),
+        checkout: new Date('2020-06-22'),
         guest: {
-          name: "Ginger Rothstein",
-          phone: "5561981494249"
+          name: 'Ginger Rothstein',
+          phone: '5561981494249'
         },
-        color: "#f54242"
+        color: '#f54242'
       },
       {
-        checkin: new Date("2020-11-09"),
-        checkout: new Date("2020-11-10"),
+        checkin: new Date('2020-11-20'),
+        checkout: new Date('2020-11-21'),
         guest: {
-          name: "Frank Navarro",
-          phone: "5561981494249"
+          name: 'Frank Navarro',
+          phone: '5561981494249'
         },
-        color: "#f54242"
+        color: '#f54242'
       }
       ],
-      "occurrancies": [
+      occurrancies: [
         {
-          startDate: new Date("2020-06-17"),
-          endDate: new Date("2020-06-18"),
+          startDate: new Date('2020-06-27'),
+          endDate: new Date('2020-06-28'),
           type: 'Manutenção'
         }
       ]
     },
     {
-      "id": 2,
-      "title": "QMSW 5, Lote 9",
-      "reservations": [{
-        checkin: new Date("2020-06-06"),
-        checkout: new Date("2020-06-11"),
+      id: 2,
+      title: 'QMSW 5, Lote 9',
+      reservations: [{
+        checkin: new Date('2020-06-06'),
+        checkout: new Date('2020-06-11'),
         guest: {
-          name: "Nicky Santoro",
-          phone: "5561981494249"
+          name: 'Nicky Santoro',
+          phone: '5561981494249'
         },
-        color: "#0f75f2"
+        color: '#0f75f2'
       }]
     },
-  ]
+  ];
 
   constructor() { }
 }
